d64cc7394e30371238f9a568f68f1241d419c18f
Tanguy <tanguy.magne@inf.ethz.ch>
2025-10-03 17:02:52 +0200

    changes for potracer

--- src/SLDvec/preprocessing/vectorize.py
+++ src/SLDvec/preprocessing/vectorize.py
@@ -17,36 +17,36 @@ def potrace_vectorize(binary_image: np.array) -> List[Spline]:
     """
     # Revert the binary image
     binary = binary_image.copy()
-    binary = 1 - binary.astype(int)
+    binary = binary.astype(int) * 255
 
     # Create a bitmap from the array
     bmp = potrace.Bitmap(binary)
 
     # Trace the bitmap to a path
-    path = bmp.trace()
+    plist = bmp.trace()
 
     # Extract all the bezier control points from the path
     points = []
-    for curve in path.curves:
+    for curve in plist:
         points.append([])
-        points[-1].append([curve.start_point])
+        points[-1].append([[curve.start_point.x, curve.start_point.y]])
 
-        for segment in curve:
+        for segment in curve.segments:
             if segment.is_corner:
-                points[-1][-1].append(segment.c)
-                points[-1][-1].append(segment.c)
-                points[-1][-1].append(segment.end_point)
+                points[-1][-1].append([segment.c.x, segment.c.y])
+                points[-1][-1].append([segment.c.x, segment.c.y])
+                points[-1][-1].append([segment.end_point.x, segment.end_point.y])
             else:
-                points[-1][-1].append(segment.c1)
-                points[-1][-1].append(segment.c2)
-                points[-1][-1].append(segment.end_point)
+                points[-1][-1].append([segment.c1.x, segment.c1.y])
+                points[-1][-1].append([segment.c2.x, segment.c2.y])
+                points[-1][-1].append([segment.end_point.x, segment.end_point.y])
 
-            points[-1].append([segment.end_point])
+            points[-1].append([[segment.end_point.x, segment.end_point.y]])
 
         points[-1].pop(-1)
 
     # Remove empty curves
-    points = [np.array(curve) for curve in points if len(curve) > 0]
+    points = [np.array(curve).astype(float) for curve in points if len(curve) > 0]
 
     # Convert the bezier curves to Spline objects
     curves = [Spline(bezier_curve) for bezier_curve in points if len(bezier_curve) > 0]
